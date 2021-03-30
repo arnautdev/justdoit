2e370b6d2f9751b6ee9fe10c42c6712ee91062d8
dmitry <d.arnaut@credissimo.bg>
2021-03-30 17:40:13 +0300

    update migration

--- database/migrations/2021_03_20_185258_create_permission_tables.php
+++ database/migrations/2021_03_20_185258_create_permission_tables.php
@@ -22,8 +22,8 @@ class CreatePermissionTables extends Migration
 
         Schema::create($tableNames['permissions'], function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('guard_name');
+            $table->string('name', 100);
+            $table->string('guard_name', 100);
             $table->timestamps();
 
             $table->unique(['name', 'guard_name']);
@@ -31,8 +31,8 @@ class CreatePermissionTables extends Migration
 
         Schema::create($tableNames['roles'], function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('guard_name');
+            $table->string('name', 100);
+            $table->string('guard_name', 100);
             $table->timestamps();
 
             $table->unique(['name', 'guard_name']);
@@ -51,7 +51,7 @@ class CreatePermissionTables extends Migration
                 ->onDelete('cascade');
 
             $table->primary(['permission_id', $columnNames['model_morph_key'], 'model_type'],
-                    'model_has_permissions_permission_model_type_primary');
+                'model_has_permissions_permission_model_type_primary');
         });
 
         Schema::create($tableNames['model_has_roles'], function (Blueprint $table) use ($tableNames, $columnNames) {
@@ -67,7 +67,7 @@ class CreatePermissionTables extends Migration
                 ->onDelete('cascade');
 
             $table->primary(['role_id', $columnNames['model_morph_key'], 'model_type'],
-                    'model_has_roles_role_model_type_primary');
+                'model_has_roles_role_model_type_primary');
         });
 
         Schema::create($tableNames['role_has_permissions'], function (Blueprint $table) use ($tableNames) {
